824d039a0bec8584a0c0d092178e4aeb02c174d6
Joao Pedro Resque <jpresque@gmail.com>
2022-10-10 15:21:48 -0300

    feat: Criação do Models e Schemas de Cadastro

--- app/models/pessoa.py
+++ app/models/pessoa.py
@@ -4,13 +4,12 @@ from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 
 from settings.db import *
 
-
 class Pessoa(Base):
-    _tablename_ = 'Pessoa'
-    email = Column(String(50), primary_key=True)
-    id = Column(Integer)
-    nome = Column(String(50))
-    sobrenome = Column(String(50))
-    celular = Column(Integer)
-    senha = Column(String(10))
-    conf_senha = Column(String(10))
+_tablename_ = 'Pessoa'
+email = Column(String(50), primary_key=True)
+id = Column(Integer)
+nome = Column(String(50))
+sobrenome = Column(String(50))
+celular = Column(Integer)
+senha = Column(String(10))
+conf_senha = Column(String(10))
