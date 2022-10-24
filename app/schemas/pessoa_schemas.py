da23cd1518b8691a1bc18232848212b790e3eb74
Luccas Souza <emaildocastro.adm01@gmail.com>
2022-10-23 23:27:07 -0300

    refactor: reajuste do models e schemas do usuario

---
+++ app/schemas/pessoa_schemas.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr, HttpUrl
+
+class Imagem(BaseModel):
+    url: HttpUrl
+    nome: str
+
+class UsuarioSchemaBase(BaseModel):
+    id: Optional[int] = None
+    email: EmailStr
+    nome : str
+    sobrenome : str
+    celular : str
+    imagem : Imagem | None = None
+
+    class Config:
+        orm_mode = True
+
+
+class UsuarioSchemaCreate(UsuarioSchemaBase):
+    senha: str
+
+
+class UsuarioSchemaUp(UsuarioSchemaBase):
+    email: Optional[EmailStr]
+    senha: Optional[str]
+    nome : Optional[str]
+    sobrenome : Optional[str]
+    celular : Optional[str]
+    imagem : Optional[Imagem]
