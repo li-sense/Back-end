617865b3a34e17d6272a993ce890801695c383d9
Joao Pedro Resque <jpresque@gmail.com>
2022-10-24 21:51:17 -0300

    refactor: Reajuste da models e schemas do produto

--- app/models/product_models.py
+++ app/models/product_models.py
@@ -12,3 +12,20 @@ class ProductModel(settings.DBBaseModel):
     descricao: str = Column(String(256), nullable=False)
     preco: float = Column(Float, nullable=False)
     detalhes: str = Column(String(500), nullable=False)
+
+    imagem = relationship("ImagensModel", back_populates="produtos")
+
+
+class ImagensModel(settings.DBBaseModel):
+    __tablename__ = "imagens"
+    id: int = Column(Integer, primary_key=True, autoincrement=True)
+    url: str = Column(String(256))
+    nome: str = Column(String(256))
+    id_produto: int = Column(Integer, ForeignKey("produtos.id"))
+    quantidade: int = Column(Integer, primary_key=False, autoincrement=False)
+    descricao: str = Column(String(256))
+    preco : int = Column(Integer, primary_key=False, autoincrement=True)
+    detalhes : str = Column(String(256))
+
+    produto = relationship("ProductModel", back_populates="imagens")
+
