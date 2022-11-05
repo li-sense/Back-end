eda0b61824faa787947ce7dd6cea8622e2290d55
Guilherme <gdasilvarosario@gmail.com>
2022-11-05 14:53:38 -0300

    feat:finalizando implementacao adicionar imagem de perfil de usuario

--- app/routes/api_config.py
+++ app/routes/api_config.py
@@ -2,7 +2,7 @@
 from fastapi import APIRouter
 
 
-from .v1_endpoint import usuario_routes, produto_routes, vendedor_routes,image_routes
+from .v1_endpoint import usuario_routes, produto_routes, vendedor_routes,imagem_usuario_routes
 
 
 
@@ -10,6 +10,6 @@ api_router = APIRouter()
 
 
 api_router.include_router(usuario_routes.router, prefix='/usuarios', tags=['usuarios'])
-api_router.include_router(vendedor_routes.router, prefix='/vendedor', tags=['vendedor'])
+api_router.include_router(imagem_usuario_routes.router, prefix='/imagens', tags=['imagens'])
 api_router.include_router(produto_routes.router, prefix='/produtos', tags=['produtos'])
-api_router.include_router(image_routes.router, prefix='/imagens', tags=['imagens'])
+api_router.include_router(vendedor_routes.router, prefix='/vendedor', tags=['vendedor'])
