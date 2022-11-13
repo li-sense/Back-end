2e8d2c3a160ddd4178158c74cff50416654a2bde
vinnieoh <viniciusoliveira926@gmail.com>
2022-11-13 16:25:27 -0300

    Finalizacao do produto

--- app/models/product_models.py
+++ app/models/product_models.py
@@ -2,16 +2,15 @@ from sqlalchemy import Integer, Float, String, Column, ForeignKey
 from sqlalchemy.orm import relationship
 
 from app.config.configs import settings
-#from app.models.vendedor_models import VendedorModel
+from app.models.image_produto_models import ImagensProductModel
 
 class ProductModel(settings.DBBaseModel):
     __tablename__ = "produtos"
     
     id: int = Column(Integer, primary_key=True, autoincrement=True)
     nome: str = Column(String(256), nullable=False, unique=True)
-    quantidade: int = Column(Integer, nullable=False)
     descricao: str = Column(String(256), nullable=False)
-    preco: float = Column(Float, nullable=False)
+    preco: str = Column(String(50), nullable=False)
     detalhes: str = Column(String(500), nullable=False)
     
     vendedor_id = Column(Integer, ForeignKey('vendedor.id'))
@@ -21,6 +20,14 @@ class ProductModel(settings.DBBaseModel):
         "AvalicaoProdutosModel",
         cascade="all,delete-orphan",
         back_populates="produto",
+        uselist=True,
+        lazy="joined"
+    )
+
+    imagens_produtos = relationship(
+        "ImagensProductModel",
+        cascade="all,delete-orphan",
+        back_populates="produto_imagem",
         uselist=True,
         lazy="joined"
     )
