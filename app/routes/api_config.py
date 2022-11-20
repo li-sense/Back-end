90e5119827c2bfe89217e64464e075852a0ab1df
armanoalves <armanobarrosalvesh@gmail.com>
2022-11-20 17:17:15 -0300

    feat and refactor: Adição de novas dependencias e arquivos essenciais para o envio do certificado

--- app/routes/api_config.py
+++ app/routes/api_config.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from .v1_endpoint import (
-    certificate_routes, 
+    certificate_email, 
     usuario_routes, 
     produto_routes, 
     vendedor_routes,
@@ -19,7 +19,7 @@ api_router = APIRouter()
 api_router.include_router(usuario_routes.router, prefix='/usuarios', tags=['usuarios'])
 api_router.include_router(imagem_usuario_routes.router, prefix='/imagens-icon-usuario', tags=['imagens-icon-usuario'])
 api_router.include_router(endereco_usuario_routes.router, prefix='/endereco-usuarios', tags=['endereco-usuarios'])
-api_router.include_router(certificate_routes.router, prefix='/certificado', tags=['certificado'])
+api_router.include_router(certificate_email.router, prefix='/certificado', tags=['certificado'])
 api_router.include_router(produto_routes.router, prefix='/produtos', tags=['produtos'])
 api_router.include_router(avaliacao_produtos_routes.router, prefix='/avaliacao-produtos', tags=['avaliacao-produtos'])
 api_router.include_router(vendedor_routes.router, prefix='/vendedor', tags=['vendedor'])
---
+++ app/utils/create_certification.py
@@ -0,0 +1,21 @@
+from reportlab.lib.units import mm, inch
+from reportlab.pdfgen import canvas 
+
+
+
+A4 = (297*mm, 210*mm)
+
+cnv = canvas.Canvas("meu_pdf.pdf") 
+
+cnv.setPageSize(A4)
+
+cnv.setFont('Helvetica-Bold', 38)
+cnv.drawString(100, 500, "Certificado de Compra da licença")
+
+cnv.rect(30, 30, 780, 540)
+
+cnv.setFont('Helvetica', 26 )
+cnv.drawString(80, 300, "A empresa Li-Sense afirma por meio deste documento que o")
+cnv.drawString(80, 270,  "usuário(a) tem direito de posse da licença desse produto.")
+
+cnv.save()
