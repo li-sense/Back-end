a12584f2e84f85ef65ed0bb43e0ff4fef8331760
Vinicius de Oliveira <viniciusoliveira926@gmail.com>
2022-11-20 20:57:58 -0300

    Revert "feat: Rota de pagamento por boleto c/ atualização do histórico de compras" (#46)

--- app/routes/api_config.py
+++ app/routes/api_config.py
@@ -4,8 +4,7 @@ from .v1_endpoint import (avaliacao_produtos_routes, certificate_routes,
                           endereco_usuario_routes,
                           historico_compras_usuario_routes,
                           imagem_usuario_routes, produto_routes,
-                          usuario_routes, vendedor_routes, carrinho_routes,
-                          pagamento_bol_routes)
+                          usuario_routes, vendedor_routes, carrinho_routes)
 
 api_router = APIRouter()
 
@@ -19,5 +18,4 @@ api_router.include_router(avaliacao_produtos_routes.router, prefix='/avaliacao-p
 api_router.include_router(vendedor_routes.router, prefix='/vendedor', tags=['vendedor'])
 api_router.include_router(carrinho_routes.router, prefix='/carrinho', tags=['carrinho'])
 api_router.include_router(historico_compras_usuario_routes.router, prefix='/historico-compras-usuario', tags=['historico-compras-usuario'])
-api_router.include_router(pagamento_bol_routes.router, prefix='/pagamento-boleto', tags=['pagamento-boleto'])
 
