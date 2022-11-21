a92a7ff0c4f23e0a0da561a2e9c8c05202b9f02e
marcos1079 <marcos.antonio2306@outlook.com>
2022-11-20 21:34:38 -0300

    refactor:endereco

--- app/routes/v1_endpoint/endereco_usuario_routes.py
+++ app/routes/v1_endpoint/endereco_usuario_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, status, Depends, HTTPException, Response
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
@@ -6,16 +6,19 @@ from sqlalchemy.future import select
 from typing import List
 
 from app.models.endereco_models import EnderecoModels
+from app.models.usuario_models import UsuarioModel
 from app.schemas.endereco_schemas import EnderecoSchema
 
-from app.config.deps import get_session
+from app.config.deps import get_session, get_current_user
 
 
 router = APIRouter()
 
 @router.post('/registra-endereco', status_code=status.HTTP_201_CREATED, response_model=EnderecoSchema)
-async def cria_endereco(endereco: EnderecoSchema, db: AsyncSession = Depends(get_session)):
-    novo_endereco: EnderecoModels = EnderecoModels(rua=endereco.rua, numero=endereco.numero, bairro=endereco.bairro, cep=endereco.cep)
+async def cria_endereco(endereco: EnderecoSchema, db: AsyncSession = Depends(get_session), logado: UsuarioModel = Depends(get_current_user)):
+    novo_endereco: EnderecoModels = EnderecoModels(rua=endereco.rua, numero=endereco.numero, bairro=endereco.bairro, 
+                                                    cep=endereco.cep, usuario_id=logado.id)
+
     async with db as session:
         try:
             session.add(novo_endereco)
@@ -24,6 +27,7 @@ async def cria_endereco(endereco: EnderecoSchema, db: AsyncSession = Depends(get
             return novo_endereco
         except:
             raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail='Não foi possível cadastrar este endereço!')
+
 
 @router.get('/enderecos', response_model=List[EnderecoSchema])
 async def get_enderecos(db: AsyncSession = Depends(get_session)):
@@ -34,10 +38,11 @@ async def get_enderecos(db: AsyncSession = Depends(get_session)):
 
         return enderecos
 
-@router.get('/{cep}', response_model=EnderecoSchema, status_code=status.HTTP_200_OK)
-async def get_endereco(cep: str, db: AsyncSession = Depends(get_session)):
+
+@router.get('/{endereco_id}', response_model=EnderecoSchema, status_code=status.HTTP_200_OK)
+async def get_endereco(endereco_id: str, db: AsyncSession = Depends(get_session)):
     async with db as session:
-        query = select(EnderecoModels).filter(EnderecoModels.cep == cep)
+        query = select(EnderecoModels).filter(EnderecoModels.id == endereco_id)
         result = await session.execute(query)
         endereco: EnderecoModels = result.scalars().unique().one_or_none()
 
@@ -45,3 +50,47 @@ async def get_endereco(cep: str, db: AsyncSession = Depends(get_session)):
             return endereco
         else:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Endereõ não encontrado.')
+
+
+@router.put("/{endereco_id}", response_model=EnderecoSchema, status_code=status.HTTP_202_ACCEPTED)
+async def put_endereco(endereco_id: int, endereco: EnderecoSchema, logado: UsuarioModel = Depends(get_current_user), db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query = select(EnderecoModels).filter(EnderecoModels.id == endereco_id)
+        result = await session.execute(query)
+        endereco_up: EnderecoModels = result.scalars().unique().one_or_none()
+
+        if endereco_up:
+            if endereco.rua:
+               endereco_up.rua = endereco.rua
+
+            if endereco.numero:
+                endereco_up = endereco.rua
+
+            if endereco.bairro:
+                endereco_up = endereco.bairro
+
+            if endereco.cep:
+                endereco_up = endereco.cep
+
+            await session.commit()
+
+            return endereco    
+        else:
+            raise HTTPException(detail='Endereco não encontrado', status_code=status.HTTP_404_NOT_FOUND)
+
+
+
+@router.delete("/{endereco_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def del_endereco(endereco_id: int, db: AsyncSession = Depends(get_session), logado: UsuarioModel = Depends(get_current_user)):
+    async with db as session:
+        query = select(EnderecoModels).filter(EnderecoModels.id == endereco_id).filter(EnderecoModels.usuario_id == logado.id)
+        result = await session.execute(query)
+        vendedor_del: EnderecoModels = result.scalars().unique().one_or_none()
+
+        if vendedor_del:
+            await session.delete(vendedor_del)
+            await session.commit()
+
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+        else:
+            raise HTTPException(detail='Endereco não encontrado', status_code=status.HTTP_404_NOT_FOUND)
