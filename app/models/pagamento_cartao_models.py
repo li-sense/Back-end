d405a140405aaafd0e9ffc0b4c10bf357ecea09e
vinnieoh <viniciusoliveira926@gmail.com>
2022-11-25 11:10:46 -0300

    Release S04: Finalizado

--- app/models/pagamento_cartao_models.py
+++ app/models/pagamento_cartao_models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
+from app.config.configs import settings
 
 
 class PagamentoCartaoModel(settings.DBBaseModel):
