b503f6d264eec4c09918801fde3c3306b5d05383
amaur1mmj@gmail.com <amaur1mmj@gmail.com>
2022-12-08 13:55:47 -0300

    correcao de cors

--- main.py
+++ main.py
@@ -6,14 +6,9 @@ from app.config.configs import settings
 
 app = FastAPI(title=' API - Li-Sense')
 
-#Cors Config
-origins = [
-    "http://localhost",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000/api/v1/",
-    "http://127.0.0.1:8000/api/v1/usuarios/login",
-    "https://dev.li-sense.xyz/docs"
-]
+# Cors Config
+origins = ["*"]
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -28,8 +23,8 @@ app.add_middleware(
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
 
-
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level='info', reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000,
+                log_level='info', reload=True)
